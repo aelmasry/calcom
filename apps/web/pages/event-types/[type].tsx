8f18cb1f3cf39018fb88da7de106ad8200eabd5d
Omar <omar.k.elgarhy@gmail.com>
2022-08-09 13:23:03 +0200

    hard code url

--- apps/web/pages/event-types/[type].tsx
+++ apps/web/pages/event-types/[type].tsx
@@ -880,7 +880,7 @@ const EventTypePage = (props: inferSSRProps<typeof getServerSideProps>) => {
                           ...(giphyThankYouPage ? { giphyThankYouPage } : {}),
                         },
                       }
-                    }, process.env.PARENT_WINDOW_DOMAIN);
+                    }, 'https://dev.techiematter.com');
                         
                     updateMutation.mutate({
                       ...input,
