feb64775edfdb49bd3a7c1f3cd800529ceafd9f4
Ali Salem <ali.elmasery@gmail.com>
2024-10-10 23:43:24 +0300

    add logo image

--- packages/emails/src/components/AddToCalendar.tsx
+++ packages/emails/src/components/AddToCalendar.tsx
@@ -131,26 +131,32 @@ export function AddToCalendar(props: { calEvent: CalendarEvent; timeZone: string
               padding: "8px",
               color: "#000",
             }}>
-            <svg
+            
+            <img src="https://icons8.com/icon/85834/google" alt="Google Calendar" style="width: 40px; height: 40px;" />
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
               fill="currentColor"
               style={{ marginTop: "-6px", display: "inline-block", height: "16px", width: "16px" }}>
               <title>Google</title>
               <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
-            </svg>
+            </svg> */}
           </a>
         </Link>
 
         <Link href={outlookLiveLink}>
           <a
+            title="Microsoft Outlook"
             style={{ margin: '0 8px', height: '40px', width: '40px', borderRadius: '4px', border: '1px solid #e5e7eb', padding: '8px', color: '#000' }}
             target="_blank"
           >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{ marginTop: '-6px', display: 'inline-block', height: '16px', width: '16px' }}>
+
+<img src="https://icons8.com/icon/100638/microsoft-outlook" alt="Microsoft Outlook" style="width: 40px; height: 40px;" />
+
+            {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" style={{ marginTop: '-6px', display: 'inline-block', height: '16px', width: '16px' }}>
               <title>Microsoft Outlook</title>
               <path d="M7.88 12.04q0 .45-.11.87-.1.41-.33.74-.22.33-.58.52-.37.2-.87.2t-.85-.2q-.35-.21-.57-.55-.22-.33-.33-.75-.1-.42-.1-.86t.1-.87q.1-.43.34-.76.22-.34.59-.54.36-.2.87-.2t.86.2q.35.21.57.55.22.34.31.77.1.43.1.88zM24 12v9.38q0 .46-.33.8-.33.32-.8.32H7.13q-.46 0-.8-.33-.32-.33-.32-.8V18H1q-.41 0-.7-.3-.3-.29-.3-.7V7q0-.41.3-.7Q.58 6 1 6h6.5V2.55q0-.44.3-.75.3-.3.75-.3h12.9q.44 0 .75.3.3.3.3.75V10.85l1.24.72h.01q.1.07.18.18.07.12.07.25zm-6-8.25v3h3v-3zm0 4.5v3h3v-3zm0 4.5v1.83l3.05-1.83zm-5.25-9v3h3.75v-3zm0 4.5v3h3.75v-3zm0 4.5v2.03l2.41 1.5 1.34-.8v-2.73zM9 3.75V6h2l.13.01.12.04v-2.3zM5.98 15.98q.9 0 1.6-.3.7-.32 1.19-.86.48-.55.73-1.28.25-.74.25-1.61 0-.83-.25-1.55-.24-.71-.71-1.24t-1.15-.83q-.68-.3-1.55-.3-.92 0-1.64.3-.71.3-1.2.85-.5.54-.75 1.3-.25.74-.25 1.63 0 .85.26 1.56.26.72.74 1.23.48.52 1.17.81.69.3 1.56.3zM7.5 21h12.39L12 16.08V17q0 .41-.3.7-.29.3-.7.3H7.5zm15-.13v-7.24l-5.9 3.54Z" />
-            </svg>
+            </svg> */}
           </a>
         </Link>
         <Link href={outlookOfficeLink}>
